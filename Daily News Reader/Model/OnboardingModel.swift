34c338c7aab061d502af230131f8cea449f3d541
enessancar <enes57751907@gmail.com>
2024-01-18 18:02:48 +0300

    app icon and news api model added

---
+++ Daily News Reader/Model/OnboardingModel.swift
@@ -0,0 +1,15 @@
+//
+//  OnboardingModel.swift
+//  Daily News Reader
+//
+//  Created by Enes Sancar on 18.01.2024.
+//
+
+import UIKit
+
+struct OnboardingItemModel{
+    let color: UIColor
+    let title: String
+    let text: String
+    let animationName: String
+}

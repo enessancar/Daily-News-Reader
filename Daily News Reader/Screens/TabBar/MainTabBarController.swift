08abcdd674ed008e8c8399972f0a70371579d5a6
enessancar <enes57751907@gmail.com>
2024-01-19 12:21:40 +0300

    tabbar created

---
+++ Daily News Reader/Screens/TabBar/MainTabBarController.swift
@@ -0,0 +1,55 @@
+//
+//  MainTabBarController.swift
+//  Daily News Reader
+//
+//  Created by Enes Sancar on 18.01.2024.
+//
+
+import UIKit
+
+final class MainTabBarController: UITabBarController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        configureTabBar()
+    }
+    
+    private func configureTabBar() {
+        
+        UITabBar.appearance().backgroundColor = .clear
+        UITabBar.appearance().tintColor = .purple1
+        UITabBar.appearance().unselectedItemTintColor = .systemGray
+        
+        viewControllers = [
+            createNavController(for: HomeVC(),
+                                title: "Home",
+                                imageName: "house",
+                                selectedImage: "house.fill"),
+            
+            createNavController(for: SearchVC(),
+                                title: "Search",
+                                imageName: "magnifyingglass"),
+            
+            createNavController(for: FavoritesVC(),
+                                title: "Favorites",
+                                imageName: "bookmark"),
+            
+            createNavController(for: ProfileVC(),
+                                title: "Profile",
+                                imageName: "person.crop.circle")
+        ]
+    }
+    
+    fileprivate func createNavController(for viewController: UIViewController, title: String, imageName: String, selectedImage: String? = nil) -> UIViewController {
+        
+        let navController = UINavigationController(rootViewController: viewController)
+        navController.navigationBar.prefersLargeTitles = true
+        viewController.navigationItem.title = title
+        viewController.view.backgroundColor = .systemBackground
+        navController.tabBarItem.title = title
+        navController.tabBarItem.image = UIImage(systemName: imageName)
+        navController.tabBarItem.selectedImage = UIImage(systemName: selectedImage)
+        return navController
+    }
+}
+

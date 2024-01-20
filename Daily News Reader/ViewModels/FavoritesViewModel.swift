133b0aeb3a86e3330be58f9b0a16fb6971849c9c
enessancar <enes57751907@gmail.com>
2024-01-20 12:42:23 +0300

    favorites view model and onboarding view model

--- Daily News Reader/ViewModels/FavoritesViewModel.swift
+++ Daily News Reader/ViewModels/FavoritesViewModel.swift
@@ -6,3 +6,42 @@
 //
 
 import Foundation
+import FirebaseFirestore
+import FirebaseAuth
+
+final class FavoritesViewModel {
+    let currentUser = Auth.auth().currentUser?.uid
+    
+    func fetchFavorites(completion: @escaping([News]) -> ()) {
+        if let currentUser {
+            Firestore.firestore().collection("UsersInfo")
+                .document(currentUser)
+                .collection("favorites")
+                .getDocuments { snaphot, error in
+                    if let error {
+                        print(error.localizedDescription)
+                        return
+                    }
+                    guard let documents = snaphot?.documents else { return }
+                    let news = documents.compactMap({try? $0.data(as: News.self)})
+                    completion(news)
+                }
+        }
+    }
+    
+    func removeFromFavorites(news: News) {
+        if let currentUser {
+            Firestore.firestore()
+                .collection("UsersInfo")
+                .document(currentUser)
+                .collection("favorites")
+                .document(news._url.hash.description)
+                .delete { error in
+                    if let error {
+                        print(error.localizedDescription)
+                    }
+                }
+        }
+        
+    }
+}
--- Daily News Reader/ViewModels/OnboardingViewModel.swift
+++ Daily News Reader/ViewModels/OnboardingViewModel.swift
@@ -8,9 +8,24 @@
 import UIKit
 
 final class OnboardingViewModel {
-    let sliderData = OnboardingItemModel(
-        color: <#T##UIColor#>,
-        title: <#T##String#>,
-        text: <#T##String#>,
-        animationName: <#T##String#>)
+    
+    let sliderData: [OnboardingItemModel] = [
+        OnboardingItemModel(
+            color: .purple2,
+            title: "Step into the World of News",
+            text: "Open the doors to the world of news. Discover the latest headlines and stories that pique your interest.",
+            animationName: "a1"),
+        
+        OnboardingItemModel(
+            color: .purple2,
+            title: "Personalize and Explore",
+            text: "Choose the topics that matter to you and create a personalized news feed. Exploring interesting content has never been easier.",
+            animationName: "a2"),
+        
+        OnboardingItemModel(
+            color: .purple2,
+            title: "Stay Informed",
+            text: "Don't miss the latest updates. Stay informed about breaking news with instant notifications and always stay up-to-date.",
+            animationName: "a3"),
+    ]
 }

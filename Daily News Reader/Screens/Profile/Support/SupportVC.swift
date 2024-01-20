d9d89755e3261de00b0c259bcc392191219901a6
enessancar <enes57751907@gmail.com>
2024-01-20 19:36:42 +0300

    support vc and view added

--- Daily News Reader/Screens/Profile/Support/SupportVC.swift
+++ Daily News Reader/Screens/Profile/Support/SupportVC.swift
@@ -8,4 +8,20 @@
 import UIKit
 import SnapKit
 
-
+final class SupportVC: UIViewController {
+    
+    private let supportView = SupportView()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        setupView()
+    }
+    
+    private func setupView() {
+        view.addSubview(supportView)
+        
+        supportView.snp.makeConstraints { make in
+            make.edges.equalToSuperview()
+        }
+    }
+}
--- Daily News Reader/Screens/Profile/Support/SupportView.swift
+++ Daily News Reader/Screens/Profile/Support/SupportView.swift
@@ -20,14 +20,16 @@ final class SupportView: UIView {
     private lazy var headLabel = TitleLabel(textAlignment: .center, fontSize: 25)
     private lazy var descriptionLabel = SecondaryTitleLabel(fontSize: 20)
     
-    private lazy var user = HelpAndSupportView(userEmail: "enes57@gmail.com")
+    private lazy var userView = HelpAndSupportView(userEmail: "enes57@gmail.com")
     
     //MARK: - Init
     override init(frame: CGRect) {
         super.init(frame: frame)
-        
         backgroundColor = .systemBackground
+        
         configureImageView()
+        configureHeadText()
+        configureUserView()
     }
     
     required init?(coder: NSCoder) {
@@ -38,7 +40,42 @@ final class SupportView: UIView {
         addSubview(imageView)
         
         imageView.snp.makeConstraints { make in
-            make.top.equalTo(safeAreaLayoutGuide.snp.top)
+            make.top.equalTo(safeAreaLayoutGuide.snp.top).offset(10)
+            make.leading.trailing.equalToSuperview()
+            make.height.equalTo(250)
+        }
+    }
+    
+    private func configureHeadText() {
+        addSubviews(headLabel, descriptionLabel)
+        
+        headLabel.text = "Get In Touch"
+        descriptionLabel.text = "If you have any inquiries get in touch with us. We'll be happy to help you"
+        
+        headLabel.snp.makeConstraints { make in
+            make.top.equalTo(imageView.snp.bottom).offset(8)
+            make.centerX.equalToSuperview()
+        }
+        
+        descriptionLabel.numberOfLines = 2
+        descriptionLabel.textAlignment = .center
+        
+        descriptionLabel.snp.makeConstraints { make in
+            make.top.equalTo(headLabel.snp.bottom).offset(20)
+            make.leading.equalToSuperview().offset(20)
+            make.trailing.equalToSuperview().offset(-20)
+        }
+    }
+    
+    private func configureUserView() {
+        addSubview(userView)
+        
+        userView.snp.makeConstraints { make in
+            make.top.equalTo(descriptionLabel.snp.bottom).offset(30)
+            make.leading.equalToSuperview().offset(20)
+            make.trailing.equalToSuperview().offset(-20)
+            make.height.equalTo(100)
+            make.centerX.equalToSuperview()
         }
     }
 }
